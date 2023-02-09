f9fdc210877955dca987742fb5352dd527bd25a7
Daniel Tse <danieltse@gmail.com>
2023-02-09 17:28:59 +1100

    Build gradle updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
 
 plugins {
     id ("java")
-//    application
     id ("org.openapi.generator") version "6.2.1"
     id ("org.springframework.boot") version "3.0.2"
     id ("io.spring.dependency-management") version "1.1.0"
@@ -16,7 +15,6 @@ plugins {
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.allopen") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
-//    id ("org.jetbrains.kotlin.plugin.noarg") version "1.8.0"
 }
 
 group = "playground"
@@ -25,25 +23,40 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://packages.confluent.io/maven")
+    }
 }
 
 dependencies {
 //    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
 
-    implementation("org.springframework.boot:spring-boot-starter-web") {
-        exclude(group = "org.slf4j", module = "slf4j-log4j12")
-    }
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.kafka:spring-kafka")
 
-//    implementation("org.apache.avro:avro-tools:1.11.1")
-    implementation("org.apache.avro:avro:1.11.1")
+    implementation("org.apache.avro:avro-tools:1.11.1") {
+//        exclude(group = "ch.qos.logback", module = "logback-classic")
+//        exclude(group = "org.slf4j", module = "slf4j-reload4j")
+    }
+//    implementation("org.apache.avro:avro:1.11.1")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.confluent:kafka-avro-serializer:7.3.1")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-//    testImplementation(kotlin("test"))
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "mockito-core")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("com.ninja-squad:springmockk:4.0.0")
+}
+
+configurations.all {
+    exclude(group = "ch.qos.logback", module = "logback-classic")
 }
 
 tasks.test {
@@ -65,7 +78,7 @@ allOpen {
 }
 
 openApiGenerate {
-    generatorName.set("kotlin")
+    generatorName.set("kotlin-spring")
     inputSpec.set("${projectDir}/src/main/resources/spec/ce-pii-v3.yaml")
     outputDir.set("${buildDir}/generated")
     apiPackage.set("org.openapi.example.api")
